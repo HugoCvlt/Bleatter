9931705b947bf884853556e5fae79443e898e1fd
HugoCvlt <hugocalvet66@icloud.com>
2023-01-31 18:27:11 +0100

    Create data_struct.py

---
+++ data_struct.py
@@ -0,0 +1,169 @@
+import hashlib
+import math
+
+
+def nextprime(n):
+    p = n+1
+    for i in range(2, p):
+        if(p % i == 0):
+            p = p+1
+    else:
+        return p
+
+
+class HashTable(object):
+    def __init__(self, nb_element):
+        self.size = nextprime(math.floor(1.3 * nb_element))
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key, len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data  # replace
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
+
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data  # replace
+
+    def hashfunction(self, key, size):
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash + 1) % size
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+
+        data = None
+        stop = False
+        found = False
+        position = startslot
+        while self.slots[position] != None and not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+
+def hashFunction(x):  # this function will hash k times the key
+    # we'll use sha256 just for this example
+    h = hashlib.sha256(str(x).encode('utf-8'))
+    return int(h.hexdigest(), base=16)
+
+
+class BloomFilter(object):
+
+    def __init__(self, m, k, hashFun):
+        self.m = m  # size of the bit vector
+        self.vector = [0] * m
+        self.k = k  # number of hashfunctions computed to store an element
+        self.data = dict()
+        self.falsePositive = 0  # number of value store inside
+        self.hashFun = hashFun
+
+    def insert(self, key, value):
+        current = key
+        for i in range(self.k):
+            current = self.hashFun(current) % self.m
+            print(current)
+            self.vector[current] = 1
+
+        self.falsePositive += 1
+        self.data[key] = value
+        # last part store the value in to a data set
+        # having access easily to the element store in the Bloom filter
+
+    def contains(self, key):
+        current = key
+        for i in range(self.k):
+            current = (self.hashFun(current) % self.m)
+
+            if (self.vector[current] != 1):
+                return float(1)  # never see this key
+
+        # How computing the false positive probability
+        return (1 - math.exp(-self.k*self.falsePositive/self.m))**self.k
+
+        # if each position in the bit vector are 1, we only can
+        # return a probability of the key being in the bloom filter.
+
+
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+
+    def to_list(self):
+        l = []
+        if self.head is None:
+            return l
+
+        node = self.head
+        while node:
+            l.append(node.data)
+            node = node.next_node
+        return l
+
+    def print_ll(self):
+        ll_string = ""
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f" {str(node.data)} ->"
+            node = node.next_node
+
+        ll_string += " None"
+        print(ll_string)
+
+    def insert_beginning(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            self.last_node = self.head
+            return
+
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.insert_beginning(data)
+            return
+
+        self.last_node.next_node = Node(data, None)
+        self.last_node = self.last_node.next_node
+
+    def concatenate(l1, l2):
+        temp = l1.head
+        while temp.next_node != None:  # find the last node
+            temp = temp.next_node
+        temp.next_node = l2.head
+        return l1
